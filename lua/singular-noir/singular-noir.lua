8f6db17985b913a308dacd2839a659bae0f9f5de
Victor Kahl Petersen <victorkahlpetersen@gmail.com>
2025-10-25 00:28:07 +0200

    Add Highlights for default groups

--- lua/singular-noir/singular-noir.lua
+++ lua/singular-noir/singular-noir.lua
@@ -3,24 +3,24 @@ local M = {}
 M.colors = {
     none = "NONE",
 
-    bg = "#000000",
-    fg = "#c1c1c1",
+    black1 = "#000000",
+    black2 = "#121212",
+    black3 = "#222222",
+
+    grey1 = "#585858",
+    grey2 = "#aaaaaa",
+    grey3 = "#c1c1c1",
+
     white = "#ffffff",
-    light_grey = "#c1c1c1",
-    grey = "#aaaaaa",
-    dark_grey = "#585858",
 
-    light_black = "#121212",
-    lighter_black = "#222222",
+    comment = "#804400", -- hsl(32, 100%, 25%)
+    orange1 = "#b35f00", -- hsl(32, 100%, 35%)
+    orange2 = "#ff8800", -- hsl(32, 100%, 50%)
+    orange3 = "#ffa033", -- hsl(32, 100%, 60%)
 
-    amber = "#ffb346",
-    vivid_orange = "#ff8800",
-    burnt_orange = "#cc7722",
-
-    error = "#d23c1f",
-    comment = "#ab955c",
-
-    test_red = "#ff0000",
+    green = "#2f7030",
+    yellow = "#b35f00",
+    red = "#a02818",
 }
 
 local function merge_highlights(...)
